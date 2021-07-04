51c517f3d5cb81b276c94b6e7ad21e702b23f537
root <root@Orange.localdomain>
2021-07-04 18:45:08 +0200

    réparation largeur footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,20 +1,15 @@
 <template>
   <section class="footerSection background">
-      <div class="footerBox">
-          <div class="leftBox threeBoxes">
-
-          </div>
-          <div class="centerBox threeBoxes">
+     
+         
+          
              
     <!-- contrôle, alt, click pour se diriger vers la page Login -->
     <img class="logoFooter" @click.ctrl.alt="login" src="https://firebasestorage.googleapis.com/v0/b/singe-diesel.appspot.com/o/images%20misc%2Flogo-carr%C3%A9-blanc.png?alt=media&token=7eae1d0d-aeda-4351-b6cf-e34ed23a564c" alt="">
 
-          </div>
-          <div class="rightBox threeBoxes">
-              
-
-          </div>
-      </div>
+          
+          
+      
 
   </section>
 </template>
@@ -43,22 +38,17 @@ export default {
 <style scoped>
     .footerSection{
         width: 100%;
-        height: 50vh;
-    }
-    .footerBox{
-        width: 100%;
         display: flex;
-        height: 100%;
+        padding: 10vh;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+
     }
-    .threeBoxes{
-        width: 33.33%;
-        height: 100%;
-    }
-    .centerBox{
-        display: grid;
-        place-items: center
-    }
+    
+    
+    
     .logoFooter{
-        height: 50%;
+        width: min(200px, 50%);
     }
 </style>
